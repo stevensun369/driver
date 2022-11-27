4397fa38186fcb0ecfeda39c6a9852fb0e05d734
Soare Catalin <stevensuncatalin369@gmail.com>
2022-11-27 04:46:32 +0200

    it works.

---
+++ src/App.svelte
@@ -0,0 +1,34 @@
+<script>
+
+	import axios from 'axios'
+
+	import {writable} from 'svelte/store'
+
+	let lat = writable("")
+	let long = writable("")
+	let loaded = writable("")
+
+	function success (pos) {
+		$lat = pos.coords.latitude;
+		$long = pos.coords.longitude;
+
+		console.log($lat + " " + $long)
+	}
+
+	setInterval(async () => {
+		navigator.geolocation.getCurrentPosition(success)
+
+		try {
+			const {data} = await axios.get(
+				`http://live.elmtree.ro/send?long=${$long}&lat=${$lat}`
+			)
+			// alert($lat + " " + $long)
+
+			$loaded = true
+		} catch (error) {
+			console.log(error)
+		}
+	}, 1000)
+
+
+</script>
